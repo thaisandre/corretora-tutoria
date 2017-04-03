1e490c6c21978433ead5c7d5f346ac31306c6404
thais andre <thaisandre.sc@gmail.com>
2017-04-03 20:25:48 -0300

    refatoracao metodo investe

--- src/br/com/corretora/Principal.java
+++ src/br/com/corretora/Principal.java
@@ -2,31 +2,46 @@ package br.com.corretora;
 
 import java.time.LocalDate;
 
-import br.com.corretora.modelo.CDB;
 import br.com.corretora.modelo.Conta;
+import br.com.corretora.modelo.Investimento;
+import br.com.corretora.modelo.TipoDeInvestimento;
 import br.com.corretora.modelo.Usuario;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		
-		Conta conta = new Conta(1000.0);
+			
+		Conta conta = new Conta(10000.0);
 		Usuario thais = new Usuario(conta);
 		LocalDate dataInicial = LocalDate.of(2017, 6, 10);
 		
-		CDB cdb = new CDB(1000.0, dataInicial);
 		
+		Investimento cdb = new Investimento(1000.0, dataInicial, 0.11, TipoDeInvestimento.CDB);
+		Investimento lci = new Investimento(1000.0, dataInicial, 0.07, TipoDeInvestimento.LCI);
+		
+		
+		//tenta investir
 		if(thais.investe(cdb)) {
-			System.out.println("investiu");
+			System.out.println("investiu no cdb");
 		} else {
 			System.out.println("não foi possível concluir o investimento");
 		}
 		
-		if(thais.investe(cdb)) {
+		if(thais.investe(lci)) {
+			System.out.println("investiu no lci");
+		} else {
+			System.out.println("não foi possível concluir o investimento");
+		}
+		
+		//listar imnvestimentos do usuario
+		for(Investimento investimento : thais.getInvestimentos()) {
+			System.out.println(investimento);
+		}
+		
+		if(thais.resgata(cdb)) {
 			System.out.println("investiu");
 		} else {
 			System.out.println("operação inválida");
-		}
+		}		
 	}
 }
