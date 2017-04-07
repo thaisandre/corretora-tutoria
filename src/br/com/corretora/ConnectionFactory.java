8adefa856ed74c811903849894aa1ed8427c7f1b
thais andre <thaisandre.sc@gmail.com>
2017-04-07 11:19:00 -0300

    conexao com o banco

---
+++ src/br/com/corretora/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package br.com.corretora;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public Connection getConnection() {
+		try {
+			return DriverManager.getConnection("jdbc:mysql://localhost/corretora", "root", "1234");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
