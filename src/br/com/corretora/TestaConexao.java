34b0ad666fe1fe320adb440f8a95d23f411e12a5
thais andre <thaisandre.sc@gmail.com>
2017-04-07 11:20:29 -0300

    testa conexao

---
+++ src/br/com/corretora/TestaConexao.java
@@ -0,0 +1,15 @@
+package br.com.corretora;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection = new ConnectionFactory().getConnection();
+		System.out.println("conexão aberta!");
+		connection.close();
+
+	}
+
+}
