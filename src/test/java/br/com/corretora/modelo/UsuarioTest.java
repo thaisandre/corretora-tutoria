f57a6aaeb11d982105a7dadbc2d3a0f1737de68d
thais <thaisandre.sc@gmail.com>
2017-05-05 20:59:10 -0300

    testes usuario

--- src/test/java/br/com/corretora/modelo/UsuarioTest.java
+++ src/test/java/br/com/corretora/modelo/UsuarioTest.java
@@ -4,37 +4,37 @@ import org.junit.Test;
 
 public class UsuarioTest {
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void usuarioNaoPodeTerNomeNulo() {
 		new Usuario (null, "joao@abc.com", "123-5");
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void usuarioNaoPodeTerLoginNulo() {
 		new Usuario ("joao", null, "123-5");
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void usuarioNaoPodeTerSenhaNula() {
 		new Usuario ("joao", "joao@abc.com", null);
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void usuarioNaoPodeTerNomeELoginNulos() {
 		new Usuario (null, null, "123-5");
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void usuarioNaoPodeTerNomeESenhaNulos() {
 		new Usuario (null, "joao@abc.com", null);
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void usuarioNaoPodeTerLoginESenhaNulos() {
 		new Usuario ("joao", null, null);
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void usuarioNaoPodeTerNomeELoginESenhaNulos() {
 		new Usuario (null, null, null);
 	}
